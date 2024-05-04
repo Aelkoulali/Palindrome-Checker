28eee9a48337cbc1a7d872f27f76a54e71a012d6
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-05-04 22:05:06 +0200

    Add Const - Get Element By Id

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Get Element By Id
+const userInput = document.getElementById('text-ipnut');
+const checkButton = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
+
+//Function to check for palindrome
