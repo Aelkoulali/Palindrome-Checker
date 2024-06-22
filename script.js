6e5ec995f86d38bfc5a965b2bdc333e87d2eebbb
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-06-22 23:38:17 +0200

    Add Function To Check For Palindrome

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const checkButton = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
 //Function to check for palindrome
+
+const checkForPalindrome = input => {
+    const originalInput = input; // Store for later output
+  
+    if (input === '') {
+      alert('Please input a value');
+      return;
+    }
+    }
