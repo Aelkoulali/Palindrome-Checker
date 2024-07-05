0ccdc4b8d6d04f2b8df62ce00486bf491fc9cb8d
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-07-05 22:10:28 +0200

    Update EventListener

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const result = document.getElementById('result');
 
 //Function Alert
 checkButton.addEventListener("click",() => {
+  const replaced = textInput.value.replace(/[^A-Za-z0-9]/g,"")
   if (textInput.value === ""){
     alert("Please input a value")
   } else if (textInput.value.length === 1){
     result.innerText = `${textInput.value} is a palindrome`
- }  else if (textInput.value.length === [...textInput.value].reverse().join("")){
+ }  else if (replaced === [...replaced].reverse().join("")){
     result.innerText = `${textInput.value} is a palindrome`
  }
 });
