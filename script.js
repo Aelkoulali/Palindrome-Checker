77baa17fac53deb02c5314eb38555a921f986863
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-07-09 16:38:40 +0200

    Add EventListener

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const result = document.getElementById('result');
 
 //Function Alert
 checkButton.addEventListener("click",() => {
-  const replaced = textInput.value.replace(/[^A-Za-z0-9]/g,"")
+  const lowerReplaced = textInput.value.toLowerCase().replace(/[^A-Za-z0-9]/g,"")
   if (textInput.value === ""){
     alert("Please input a value")
   } else if (textInput.value.length === 1){
     result.innerText = `${textInput.value} is a palindrome`
- }  else if (replaced === [...replaced].reverse().join("")){
+ }  else if (lowerReplacedreplaced === [...lowerReplaced].reverse().join("")){
     result.innerText = `${textInput.value} is a palindrome`
+ }  else {
+    result.innerText = `${textInput.value} is not a palindrome`
  }
 });
